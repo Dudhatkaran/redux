5bd033f3d4dd5fac0b2499193f196f9ba272eab3
Dudhatkaran <karanvd2002@gmail.com>
2023-02-14 18:26:57 +0530

    Create Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TodolistContainer from './Containers/TodolistContainer'
+
+function App() {
+  return (
+    <>
+      <TodolistContainer />
+    </>
+  );
+}
+
+export default App;
